47463e939c04125cd58c39538bd30a65ab09478a
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-05-05 17:22:29 -0500

    m

---
+++ Calculadora.py
@@ -0,0 +1,39 @@
+
+import operaciones
+
+
+def menu():
+    print("Calculadora Modular")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Potencia")
+    print("6. División entera")
+    print("0. Salir")
+
+while True:
+    menu()
+    opcion = input("Selecciona una operación: ")
+
+    if opcion == "0":
+        print("Hasta luego.")
+        break
+
+    a = float(input("Ingresa el primer número: "))
+    b = float(input("Ingresa el segundo número: "))
+
+    if opcion == "1":
+        print("Resultado:", operaciones.suma(a, b))
+    elif opcion == "2":
+        print("Resultado:", operaciones.resta(a, b))
+    elif opcion == "3":
+        print("Resultado:", operaciones.multiplicacion(a, b))
+    elif opcion == "4":
+        print("Resultado:", operaciones.division(a, b))
+    elif opcion == "5":
+        print("Resultado:", operaciones.potencia(a, b))
+    elif opcion == "6":
+        print("Resultado:", operaciones.division_entera(a, b))
+    else:
+        print("Opción inválida.")
