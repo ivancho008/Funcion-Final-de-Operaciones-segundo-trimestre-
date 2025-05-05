4c0feda8a8f2b676dcc3e3a8b445d5033a861e00
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-05-05 17:20:17 -0500

    Actualizar

---
+++ Operaciones.py
@@ -0,0 +1,24 @@
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: División por cero"
+
+def potencia(a, b):
+    return a ** b
+
+def division_entera(a, b):
+    if b != 0:
+        return a // b
+    else:
+        return "Error: División por cero"
